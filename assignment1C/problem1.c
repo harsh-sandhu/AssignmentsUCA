84c4866ce480e5dc93e84b1455a21adb72006668
Harsh Sandhu <harshsandhu.cse19@chitkarauniversity.edu.in>
2021-07-23 08:50:53 +0000

    Assignment 1_C

---
+++ assignment1C/problem1.c
@@ -0,0 +1,29 @@
+/*
+ *Program to swap two variable without using third variable
+ * 
+ * Complilation: gcc -0 swap_variables problem1.c
+ * execution: ./swap_variables
+ *
+ * @Harsh Sandhu, 1911981371, 23/07/2021 
+ * */
+
+#include <stdio.h>
+
+int main(){
+    //the two number a and b
+    int a,b;
+
+    //Input for the a and b
+    printf("Enter the first number(a)\n");
+    scanf("%d",&a);
+
+    printf("Enter the second number(b)\n");
+    scanf("%d",&b);
+
+    //the swaping of variables without using the third varible
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    printf("a = %d, b = %d\n",a,b);
+}
